8704b0af3e91efd7735fa296e27ca0a0688eecf2
Steven Buhrman <scbuhrma@gmail.com>
2012-03-14 21:44:00 -0400

    First commit for Lab 4 hopefully last

--- app/server.js
+++ app/server.js
@@ -1,8 +1,18 @@
 var http = require('http');
+var redis = require('redis');
 
-http.createServer(function (req, res) {
+var client = redis.createClient();
+
+http.createServer(function (req, res) {    
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
+    client.mget(['awesome','cool','rad','groovy','gnarly'],function(error,exists){
+	if(error){
+	  exists = error;
+	}else{
+	  res.end("Awesome: " + exists[0]+ " cool: " + exists[1]+ 
+	    " rad: " + exists[2] + " groovy: " + exists[3] + " gnarly: " + exists[4]);
+	}
+     });
 }).listen(3000);
 
 console.log('Server running on port 3000');
---
+++ app/twitter.js
@@ -0,0 +1,48 @@
+var twitter = require('ntwitter');
+var redis = require('redis');
+var credentials = require('./credentials.js');
+
+                                                                                                                                                                                                                   
+var client = redis.createClient();
+
+                                                                                                                                                                                            
+client.exists('awesome', function(error, exists) {
+    if(error) {
+        console.log('ERROR: '+error);
+    } else if(!exists) {
+        client.set('awesome', 0); 
+    };
+});
+
+var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
+
+t.stream(
+    'statuses/filter',
+    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
+    function(stream) {
+        stream.on('data', function(tweet) {
+            console.log(tweet.text);
+                                                                                                                                                                                  
+            if(tweet.text.match(/awesome/)) {
+                client.incr('awesome');
+            }
+ 	    	if(tweet.text.match(/cool/)) {
+                client.incr('cool');
+            }
+	    	if(tweet.text.match(/rad/)) {
+                client.incr('rad');
+            } 
+ 	    	if(tweet.text.match(/gnarly/)) {
+                client.incr('gnarly');
+            }
+	    	if(tweet.text.match(/groovy/)) {
+                client.incr('groovy');
+            }   
+        });
+    }
+);
